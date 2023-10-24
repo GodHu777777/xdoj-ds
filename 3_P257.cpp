e178077f0d87517283cb31b6f3dba1db63f7c833
Genghong Hu <godhu777777@gmail.com>
2023-10-24 22:20:56 +0800

    3_P257 something went wrong

--- 3_P257.cpp
+++ 3_P257.cpp
@@ -1,38 +1,42 @@
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
+#include<bits/stdc++.h>
+using namespace std;
+
 int main()
 {
-	char s1[101],s2[101],t[101];
-	int i,j;
-	float len1,len2,max=0;
-	int a[100][100]={0};
-	float similar;
-	gets(s1);
-	gets(s2);
-	len1=strlen(s1);
-	len2=strlen(s2);
-	
-	for(i=0;i<len1;i++)
-	{
-		for(j=0;j<len2;j++)
-		{
-			if(s1[i]==s2[j]||abs(s1[i]-s2[j])==32)
-			{
-				if(i!=0&&j!=0)
-				a[i][j]=a[i-1][j-1]+1;
-				else a[i][j]=1;
-			}
-		}
-	}
-	
-	for(i=0;i<len1;i++)
-	{
-		for(j=0;j<len2;j++)
-		{
-			if(a[i][j]>max) max=a[i][j];
-		}
-	}
-	similar=2.0*max/(len1+len2);
-	printf("%.3f",similar);
- } 
+    string s1,s2;
+    cin >> s1 >> s2;
+    //convert all the uppercase letters to lowercase in both strings
+    for(int i = 0; i < s1.size(); i++)
+        if(s1[i] >= 'A' && s1[i] <= 'Z')
+            s1[i] -= 'A' - 'a';
+    for(int i = 0; i < s2.size(); i++)
+        if(s2[i] >= 'A' && s2[i] <= 'Z')
+            s2[i] -= 'A' - 'a';
+    
+
+    //this 2-D array actually starts by 1
+    int dp[s1.size() + 5][s2.size() + 5];
+    int ans = 0;
+
+    for(int i = 0; i <= s1.size(); i++)
+    {
+        for(int j = 0; j <= s2.size(); j++)
+        {
+            if(i == 0 || j == 0)
+            {
+                //dp[0][*] or dp[*][0] is just for simplicity
+                dp[i][j] = 0;
+            }else if(s1[i - 1] == s2[j - 1])
+            {
+                //state transformation
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                ans = max(ans,dp[i][j]);
+            }else 
+                dp[i][j] = 0;
+        }
+    }    
+    cout << ans << '\n';
+    printf("%.3lf",2.0 * ans / ( s1.size() + s2.size() ) );
+
+    return 0;
+}
